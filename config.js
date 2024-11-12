014f683407c9d5001eb3952c18f97129451b6071
erikahentschross <erika.hentsch@rossvideo.com>
2024-11-12 11:13:38 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+var CONFIG = {
+    url: 'https://lightbulb.blcloud.net/blade/container/?format=json&pretty=yes', 
+    filterContainers: ['Closing Group']
+    // API_URLs: ['https://lightbulb.blcloud.net/blade/closinggroup/?format=json&listMeta=list','https://lightbulb.blcloud.net/blade/closingplaylist?format=json&listMeta=list']
+}
