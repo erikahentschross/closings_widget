1b87698dfca2be69f85638f2fe5130e7f1f06a4b
erikahentschross <erika.hentsch@rossvideo.com>
2025-03-05 15:35:51 -0500

    added mini widget

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 var CONFIG = {
     url: 'https://lightbulb.blcloud.net/blade/container/?format=json&pretty=yes', 
-    filterContainers: ['Closing Group']
+    filterContainers: ['Closing Group'], 
+    filterPlaylists: ['Ecole Scholair Montreal', 'Halifax Regional Schools', 'Hamilton Schools', 'Oklahoma City Public Schools', 'Ontario School Boards', 'Toronto Catholic SB' ]
     // API_URLs: ['https://lightbulb.blcloud.net/blade/closinggroup/?format=json&listMeta=list','https://lightbulb.blcloud.net/blade/closingplaylist?format=json&listMeta=list']
 }
